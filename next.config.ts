3a9b3b2fa38ac51f0a24872e6c7fef13c6c57ef0
Yash <>
2025-07-03 17:34:55 +0530

    setup: initial

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,32 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    reactCompiler: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: "https",
+        hostname: "assets.chanhdai.com",
+        port: "",
+      },
+    ],
+  },
+  transpilePackages: ["next-mdx-remote"],
+  allowedDevOrigins: ["chanhdai-macbook.local"],
+  async rewrites() {
+    return [
+      {
+        source: "/blog/:slug.md",
+        destination: "/blog.md/:slug",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
