f0f85d5a4be5028b16c83c1622133aebb0b5b4c9
Yash <67644300+YashM20@users.noreply.github.com>
2025-07-14 14:25:29 +0530

    fix: dmca, nav link,

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -7,7 +7,7 @@ import { Icons } from "./icons";
 
 export function SiteFooter() {
   return (
-    <footer className="max-w-screen overflow-x-hidden px-2">
+    <footer className="max-w-screen overflow-x-hidden px-2 pb-20">
       <div className="screen-line-before border-edge mx-auto border-x pt-4 md:max-w-3xl">
         <p className="text-muted-foreground mb-1 text-balance text-center font-mono text-sm">
           Inspired by tailwindcss.com && ui.shadcn.com &&{" "}
@@ -58,9 +58,7 @@ export function SiteFooter() {
             >
               llms.txt
             </a>
-
             <Separator />
-
             <a
               className="text-muted-foreground hover:text-foreground flex items-center transition-colors"
               href={`${SITE_INFO.url}/rss`}
@@ -70,11 +68,11 @@ export function SiteFooter() {
               <RssIcon className="size-4" />
               <span className="sr-only">RSS</span>
             </a>
-
             <Separator />
-
+            <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"></script>
             <a
-              className="text-muted-foreground hover:text-foreground flex transition-colors"
+              title="DMCA.com Protection Status"
+              className="dmca-badge text-muted-foreground hover:text-foreground flex transition-colors"
               href={
                 process.env.NEXT_PUBLIC_DMCA_URL ||
                 "https://www.dmca.com/ProtectionPro.aspx"
---
+++ tasks.md
@@ -0,0 +1,24 @@
+## TODO
+
+### AI Assistant
+
+- add copy and report options for ai chat
+- store the chat history
+- improve the chat response formatting (to support markdown, tables, etc.)
+- fix ctrl+/ & ctrl+k conflicting for ai assistant \* shortcuts
+- add ai assistant component as shadcn registary
+- make the chat modal resizeable
+- fix word mark logo in shortcut popup.
+
+### General
+
+- add cookie consent
+- change icon for pwa
+- display full project details
+
+## DONE
+
+- add dmca badge
+- add posthog
+- remove phone number
+- change GITHUB link from navbar
